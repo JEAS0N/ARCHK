b17bf33d585214c4d3bccc9e02bd3efec135380b
Jeason <danilkabobkov03@gmail.com>
2022-11-17 22:38:45 +0300

    |

---
+++ js/main.js
@@ -0,0 +1,6 @@
+
+const pop = document.querySelector(`.popover__content`);
+const icon_pop = document.querySelector(`.popover`);
+icon_pop.addEventListener('click', () => {
+  pop.classList.toggle(`popover__content_show`);
+});
