6d1d7484ef28df05481f28b27b7b932d9d561e0d
Jeason <danilkabobkov03@gmail.com>
2022-12-04 17:49:54 +0300

    filters&footer

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
-const pop = document.querySelector(`.popover__content`);
-const icon_pop = document.querySelector(`.popover`);
+const pop = document.querySelector(`.popover__content`)
+const icon_pop = document.querySelector(`.popover`)
 icon_pop.addEventListener('click', () => {
-  pop.classList.toggle(`popover__content_show`);
-});
+  pop.classList.toggle(`popover__content_show`)
+})
+
+const filterTime = document.querySelector(`.filter-items__times`)
+const filterRec = document.querySelector(`.filter-items__rec`)
+
+const timeSelector = document.querySelector(`.time-selectors`)
+const recSelector = document.querySelector(`.rec-selectors`)
+
+filterRec.addEventListener('click', () => {
+  recSelector.classList.toggle(`rec-selectors__show`)
+}
+)
+
+filterTime.addEventListener('click', () => {
+  timeSelector.classList.toggle(`time-selectors__show`)
+}
+)
+
